40c5310703bbf4382b0be8ee4645844246a499ff
Constantin <constantinu93@gmail.com>
2020-11-04 17:42:41 +0100

    add clarifai API for face recognition

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
 
+// API key from Clarifai
+
+const app = new Clarifai.App({
+  apiKey: '4fe1e570ee3c4976a1442112f18949c3'
+});
+
+
+// Particles for the animated particles background
 const particlesOptions = {
   particles: {
     number: {
@@ -27,9 +36,20 @@ class App extends Component {
   }
 
   onInputChange = (event) => {
-    console.log(event);
-
+    console.log(event.target.value);
   }
+
+  onButtonSubmit = () => {
+    console.log('click');
+    app.models.initModel({id: Clarifai.GENERAL_MODEL, version: "aa7f35c01e0642fda5cf400f543e7c40"})
+      .then(generalModel => {
+        return generalModel.predict("@@sampleTrain");
+      })
+      .then(response => {
+        var concepts = response['outputs'][0]['data']['concepts']
+      })
+  }
+
   render() {
     return (
       <div className="App">
@@ -39,7 +59,10 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm onInputChange={onInputChange}/>
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
         {/* <FaceRecognition /> */}
       </div>
     );
