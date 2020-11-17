eba59e1d98791b509d7fcbefc5f8338f1dc5f31d
Constantin <constantinu93@gmail.com>
2020-11-17 12:56:48 +0100

    connected register.js to backend

--- src/App.js
+++ src/App.js
@@ -39,8 +39,26 @@ class App extends Component {
       box: {},
       route: 'signin',
       isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
     }
   }
+
+  loadUser = (data) => {
+    this.setState({user: {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
+    }
+  })
+}
 
   // Calculate the Location of the face to display the face box on the right place:
   calculateFaceLocation = (data) => {
@@ -118,7 +136,7 @@ class App extends Component {
         { route === 'home'
           ? <div>
               <Logo />
-              <Rank />
+              <Rank name={this.state.user.name} entries={this.state.user.entries} />
               <ImageLinkForm
                 onInputChange={this.onInputChange}
                 onButtonSubmit={this.onButtonSubmit}
@@ -127,8 +145,8 @@ class App extends Component {
             </div>
             : (
               route === 'signin'
-              ? <Signin onRouteChange={this.onRouteChange}/>
-              : <Register onRouteChange={this.onRouteChange}/>
+              ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+              : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
             )  
         }
       </div>
